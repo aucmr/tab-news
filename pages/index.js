614749a6b7c0671a80942aaa4255b906af035ce8
Auciomar Cerqueira <auciomar@gmail.com>
2024-08-01 19:47:03 +0000

    feat: juros compostos

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ function Home() {
         <div>
             <h1>Bem vindos!</h1>
             <p>Esta página é para vocês, otipopos da minha vida.</p>
+            <br></br>
+            <ul>
+                <li>"Os juros compostos são a oitava maravilha do mundo. Aquele que entende, ganha; aquele que não entende, paga." (Albert Einstein)</li>
+            </ul>
         </div>
     );
 }
