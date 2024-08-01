9c659bf38c3c63e9a003c95a991a178d78c2bf11
Auciomar Cerqueira <auciomar@gmail.com>
2024-08-01 19:35:27 +0000

    refact: improve home component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Wellcome</h1>
+    return (
+        <div>
+            <h1>Bem vindos!</h1>
+            <p>Esta página é para vocês, otipopos da minha vida.</p>
+        </div>
+    );
 }
 
 export default Home
