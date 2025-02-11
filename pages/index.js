f907b3f77e0f118acec9159332650b31e79881e8
Auciomar Cerqueira <auciomar.cerqueira@serpro.gov.br>
2025-02-11 19:36:43 +0000

    feat: add daily lecture

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,60 @@
 function Home() {
-    return (
-        <div>
-            <h1>Bem vindos!</h1>
-            <p>Esta página é para vocês, otipopos da minha vida.</p>
-            <br></br>
-            <ul>
-                <li>"Os juros compostos são a oitava maravilha do mundo. Aquele que entende, ganha; aquele que não entende, paga." (Albert Einstein)</li>
-                <li>"O sucesso não é sobre quanto dinheiro você ganha, mas sobre o impacto que você cria."</li>
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Bem vindos!</h1>
+      <p>Esta página é para vocês, otipopos da minha vida.</p>
+      <p>Pensamentos:</p>
+      <ul>
+        <li>
+          "Os juros compostos são a oitava maravilha do mundo. Aquele que
+          entende, ganha; aquele que não entende, paga." (Albert Einstein)
+        </li>
+        <li>
+          "O sucesso não é sobre quanto dinheiro você ganha, mas sobre o impacto
+          que você cria."
+        </li>
+      </ul>
+
+      <h2>Leitura Diária:</h2>
+      <ul>
+        <li>
+          <a href="https://g1.globo.com/mg/minas-gerais/" target="_blank">
+            G1 MG
+          </a>
+        </li>
+        <li>
+          <a href="https://www.folha.uol.com.br" target="_blank">
+            Folha
+          </a>
+        </li>
+        <li>
+          <a href="https://valor.globo.com/" target="_blank">
+            Valor Economico
+          </a>
+        </li>
+        <li>
+          <a href="https://www.coindesk.com/" target="_blank">
+            Coin Desk
+          </a>
+        </li>
+        <li>
+          <a href="https://www.infomoney.com.br/" target="_blank">
+            infomonney
+          </a>
+        </li>
+        <li>
+          <a href="https://www.nytimes.com/" target="_blank">
+            New York Times
+          </a>
+        </li>
+        <li>
+          <a href="https://comunidade.auvp.com.br/" target="_blank">
+            Comunidade AUVP
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
